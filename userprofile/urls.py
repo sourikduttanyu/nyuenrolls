93cdb9380bc1f7950f9e26c88144a207c102027e
Janardhana Reddy M S <76699907+JANARDHANAREDDYMS@users.noreply.github.com>
2024-10-04 18:13:30 -0400

    M

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path("",views.userinfo,name="userinfo"),
     path('user_info/', views.user_info_view, name='user_info_form'),
-    path('login/',views.login_request,name='login_request'),
-    path('register/',views.register_request,name="register_request")
+    path('login/',views.login_request,name='login'),
+    path('register/',views.register_request,name='register')
 
 
 ]
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.template import loader
 from .forms import UserInfoForm, create_user_form
 from django.contrib.auth.forms import UserCreationForm 
+from django.contrib import messages
 
 def userinfo(request):
     return HttpResponse("Hello World")
@@ -12,7 +13,8 @@ def user_info_view(request):
         form = UserInfoForm(request.POST)
         if form.is_valid():
             form.save()  # Save the data to the database
-            form = UserInfoForm()    
+            form = UserInfoForm
+               
     else:
         form = UserInfoForm()
 
@@ -22,15 +24,18 @@ def login_request(request):
     context = {}
     return render(request,'login.html',context)
 
-def register_request(request):
+def register_request(request): 
     form = create_user_form()
 
     if request.method =="POST":
         form =  create_user_form(request.POST)
         if form.is_valid():
             form.save()
-
-
+            user_name = form.cleaned_data.get('username')
+            messages.success(request, 'Account for' +user_name+ 'created' )
+            return redirect('login')
+    else:
+        form = UserInfoForm()
 
     context ={'form': form}
     return render(request,'register.html',context)
