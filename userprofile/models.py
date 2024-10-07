260497d39f7cf955565aa3d38adb51ff29f57c71
Janardhana Reddy M S <76699907+JANARDHANAREDDYMS@users.noreply.github.com>
2024-10-07 13:58:04 -0400

    Updated models adding TA

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,21 +1,21 @@
 from django.db import models
 
 class StudentInfo(models.Model):
-    Edu_levels = [("Undergraduate","Undergraduate"),
-                  ("Graduate","Graduate"),
-                  ("PHD","PHD")]
-    Schools = [("Tandon","Tandon"),
-               ("Stern","Stern"),
-               ("Tisch","Tisch"),
-               ("Gallatin","Gallatin")]
+    Edu_levels = [("Undergraduate", "Undergraduate"),
+                  ("Graduate", "Graduate"),
+                  ("PHD", "PHD")]
+    Schools = [("Tandon", "Tandon"),
+               ("Stern", "Stern"),
+               ("Tisch", "Tisch"),
+               ("Gallatin", "Gallatin")]
     
     N_id = models.CharField(max_length=8, primary_key=True)
     Name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     Education_Level = models.CharField(max_length=50, choices=Edu_levels)
-    Phone_no = models.IntegerField()
+    Phone_no = models.CharField(max_length=15)  
     School = models.CharField(max_length=50, choices=Schools)
-    ta_course = models.ForeignKey('CourseInfo', null=True, blank=True, on_delete=models.SET_NULL)
+    ta_course = models.ForeignKey('CourseInfo', null=True, blank=True, related_name='tas', on_delete=models.SET_NULL)
     is_ta = models.BooleanField(default=False)
     course_enrolled = models.ManyToManyField('CourseInfo', related_name='enrolled_students')
 
@@ -27,7 +27,6 @@ class CourseInfo(models.Model):
     phd_course_capacity = models.IntegerField()
     class_day = models.DateField()
     class_time = models.TimeField()
-    ta_students = models.ManyToManyField(StudentInfo, related_name='ta_students')
     description = models.CharField(max_length=1000)
     credits = models.DecimalField(decimal_places=1, max_digits=3)
 
@@ -35,12 +34,13 @@ class FacultyInfo(models.Model):
     faculty_id = models.CharField(max_length=8, primary_key=True)
     Name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
-    Phone_no = models.IntegerField()
-    ta_students = models.ManyToManyField(StudentInfo, related_name='faculty_tas')
-    courses_taught = models.ManyToManyField(CourseInfo, related_name="instructors")
+    Phone_no = models.CharField(max_length=15) 
+    ta_students = models.ManyToManyField(StudentInfo, through='TA', related_name='faculty_tas')
 
-class AdminInfo(models.Model):
-    admin_id = models.CharField(primary_key=True)
-    Name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
-    Phone_no = models.IntegerField(max_length=12)
+class TA(models.Model):
+    student = models.OneToOneField(StudentInfo, on_delete=models.CASCADE)
+    course = models.ForeignKey(CourseInfo, on_delete=models.CASCADE)
+    faculty = models.ForeignKey(FacultyInfo, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('student', 'course')  
